fb984d9e188cf01e387591684011a1e036c8346e
anacatarinafg <anacatarinafgg@gmail.com>
2024-02-29 22:43:04 +0000

    first commit

---
+++ pw.py
@@ -0,0 +1,28 @@
+import random 
+
+# Create strings that contain all type of letters used for pws
+uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+lowercase_letters = uppercase_letters.lower()
+digits = '0123456789'
+symbols = '()[]{},;.:-$#"&%!\|/?!+º~'
+
+upper, lower, nums, syms = True, True, True, True
+
+# Create a string will all the things we are going to use
+all_chars = ''
+
+if upper: 
+    all_chars += uppercase_letters
+if lower:
+    all_chars += lowercase_letters
+if nums:
+    all_chars += digits
+if syms:
+    all_chars += symbols
+
+length = int(input('Enter the length you want the password to have.\n')) # Size of pw
+amount = int(input('Enter the amount of characters you want the password to have.\n')) # The amount of chars that the pw will have
+
+for i in range(amount): # Go to all the chars and randomize a password based on the length inserted by the user 
+    password = ''.join(random.sample(all_chars, length))
+    print(password)
