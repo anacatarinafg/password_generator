607c882e271ab0be837e070ee3bbe466e05160e0
anacatarinafg <anacatarinafgg@gmail.com>
2024-03-01 13:56:16 +0000

    fixing mistake

--- pw.py
+++ pw.py
@@ -12,7 +12,7 @@ symbols = '()[]{},;.:-$#"&%!\|/?!+º~'
 # Do we want the password to have Symbols? Yes | True
 upper, lower, nums, syms = True, True, False, True
 
-# Create a string will all the things we are going to use
+# Create a string with all the things we are going to use
 all_chars = ''
 
 if upper: 
@@ -24,9 +24,9 @@ if nums:
 if syms:
     all_chars += symbols
 
-length = int(input('Enter the length you want the password to have.\n')) # Size of pw
-amount = int(input('Enter the amount of characters you want the password to have.\n')) # The amount of chars that the pw will have
+length = int(input('Enter the length you want the password to have:')) # Size of pw
+amount = int(input('Enter the amount of password you want to generate:')) # The amount of pws that will be generated
 
-for i in range(amount): # Go to all the chars and randomize a password based on the length inserted by the user 
+for i in range(amount): # Go to all the chars and randomize a password based on the length and amount inserted by the user 
     password = ''.join(random.sample(all_chars, length))
     print(password)
